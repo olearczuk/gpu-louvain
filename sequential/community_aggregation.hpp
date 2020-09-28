3b73a71422165301e5749f2c23e3e8161d4561b7
olearczuk <olearczuk.szymon@gmail.com>
2020-09-28 18:26:49 +0200

    README

--- sequential/community_aggregation.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __COMMUNITY_AGGREGATION__HPP__
-#define __COMMUNITY_AGGREGATION__HPP__
-#include "utils.hpp"
-
-void aggregate_communities(data_structures& structures);
-
-#endif /* __COMMUNITY_AGGREGATION__HPP__ */
